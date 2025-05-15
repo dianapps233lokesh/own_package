061a8c1dce0ecda95a95105cda7bbae98e301fa1
lokesh <lokesh.saini@dianapps.com>
2025-05-15 17:34:43 +0530

    new package

--- own_package/__init__.py
+++ own_package/__init__.py
@@ -1,1 +1,1 @@
-from .core import core
+from .core import reverse_str
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="jakrta765bgt",
+    name="absnjw456tbh",
     version="0.1.0",
     packages=find_packages(),
     install_requires=[],
